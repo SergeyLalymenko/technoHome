4b094e7523262cefd3f60109fb1fa00cd5d895d1
alexandrovichdigtyar <bogdan.digtyar@gmail.com>
2022-09-11 18:49:29 +0300

    added firebase config

---
+++ fb-config.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAat4dfyrZd6cnQELABFFfcD3cCp-Lo-5s",
+  authDomain: "technohome-5688e.firebaseapp.com",
+  projectId: "technohome-5688e",
+  storageBucket: "technohome-5688e.appspot.com",
+  messagingSenderId: "1003157394130",
+  appId: "1:1003157394130:web:38fddc9e7036c29c91530b"
+};
+
+const app = initializeApp(firebaseConfig);
